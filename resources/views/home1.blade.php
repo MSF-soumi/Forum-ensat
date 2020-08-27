504987c364288bdc6b4e5d5ae91089d9af8c5457
firdaousms <firdaousmossaouir@gmail.com>
2020-08-27 14:26:26 +0200

    Modication du dashboard 1

--- resources/views/home1.blade.php
+++ resources/views/home1.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2:wght@800&display=swap" rel="stylesheet"> 
         <link href="assets/css/HomeStyle.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
     </head>
     <body>
@@ -23,19 +24,29 @@
                 <img src="assets/student.png">
             </div>
         @else
-            <nav>
+            <nav class="navbar-expand navbar-light ">
                 <img src="assets/ensat.jpg">
                 <ul>
-                    <li>Profil</li>
-                    <li>Contact</li>
-                    <li>LOG OUT</li>
+                    <li><a href="">Profil</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">LOG OUT</a></li>
                 </ul>
             </nav>
         @endguest 
-
+        
+        </br>
+        
         <div id= "dash">
             <table>
-                <tr><th>DASHBOARD</th></tr>
+                <tr><th style="font-size: 40px; ">DASHBOARD</th></tr>
+                <tr>
+                    <td><a href="#">titre</a></td>
+                    <td>stars </td>
+                </tr>
+                <tr>
+                    <td><a href="#">titre</a></td>
+                    <td>stars </td>
+                </tr>
                 <tr>
                     <td><a href="#">titre</a></td>
                     <td>stars </td>
@@ -44,5 +55,6 @@
 
 
         </div>
+    
     </body>
 </html>
