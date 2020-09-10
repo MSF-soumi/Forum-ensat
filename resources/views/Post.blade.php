35f4c4f6db8eda70c391851794d1440253e32595
firdaousms <firdaousmossaouir@gmail.com>
2020-09-10 23:20:39 +0200

    Post blade's style modification

--- resources/views/Post.blade.php
+++ resources/views/Post.blade.php
@@ -8,14 +8,14 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2:wght@800&display=swap" rel="stylesheet"> 
-        <link href="assets/css/style.css" rel="stylesheet">
+        <link href="assets/css/style.css" rel="stylesheet"> 
         <link rel="stylesheet" href= "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
     </head>
     <body>
     <div class="r1">Post</div>
     
-          <form action="{{ route('Post.create') }}" method="POST">
+          {{-- <form action="{{ route('Post.create') }}" method="POST">
           @csrf
                         <h1 >Add Your Post!</h1>
                         
@@ -23,8 +23,32 @@
  
                         <textarea  name="contenu" placeholder="Détailler votre question"></textarea>
                         <button type="submit" class="Bout">Envoyer !</button>
-                </form>
+                </form> --}}
 
+
+<form action="{{ route('Post.create') }}" method="POST">
+    @csrf
+<h1 >Add Your Post!</h1>
+<div class="form-group row">
+    <label for="titre" class="col-md-4 col-form-label text-md-right">Titre</label>
+
+    <div class="col-md-6">
+        <input id="titre" type="text" class="form-control " name="titre"  required placeholder="Titre de votre poste"  autofocus>
+
+    </div>
+    
+</div>
+<div class="form-group row">
+    
+    <label for="contenu" class="col-md-4 col-form-label text-md-right">Details</label>
+    <div class="col-md-6">
+        <input id="contenu" type="text" class="form-control "  name="contenu"  required placeholder="Detailler votre question"  autofocus >
+
+    </div>
+    
+</div>
+<button type="submit" class="Bout">Envoyer !</button>
+</form>
 
     
     </body>
